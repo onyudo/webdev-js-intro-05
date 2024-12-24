da34ff59657eb0b52bc194af799cd94b53b52c03
“Martin <martin@forceandform.com>
2024-12-24 12:02:41 -0500

    Completed experiments in index.js and commented out my code once finished. Updated README to indicate that all tasks for Part 1 of the assignment had been completed.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// Write any questions that may arise in this experiment and we can discuss them in class.
+
+// Create an array that holds different data types and log it the console. What happens? Log your arrays to the console.
+
+/*
+
+let shoppingList = [12, "cheese", true];
+console.log(shoppingList); // (3) [12, 'cheese', true]
+
+
+// Log the typeof to one array to the console. What is returned?
+
+let shoppingList = [12, "cheese", true];
+console.log(typeof shoppingList); // object?
+console.log(Array.isArray(shoppingList)); // checking if something is specifically an array. result is true.
+
+// Remove an item from your arrays.
+
+let shoppingList = [12, "cheese", true];
+shoppingList.shift();
+console.log(shoppingList); // (2) ['cheese', true]
+
+// Add an item from your arrays.
+
+let shoppingList = [12, "cheese", true];
+shoppingList.push("sauerkraut");    
+console.log(shoppingList); // (4) [12, 'cheese', true, 'sauerkraut']
+
+// Change the value of an element in each array.
+
+let shoppingList = [12, "cheese", true, "sauerkraut"];
+shoppingList[0] = 35;
+shoppingList[2] = false;
+
+console.log(shoppingList); // (4) [35, 'cheese', false, 'sauerkraut']
+
+*/
